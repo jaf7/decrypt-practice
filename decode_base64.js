21e9921cf2bbd4450fc913b7052e88994fd51884
jaf7 <janthonyfields@gmail.com>
2017-03-13 17:42:16 -0600

    Added comments to decode_base64.js

--- decode_base64.js
+++ decode_base64.js
@@ -6,6 +6,13 @@ var hEncodedString = 'Ik5ldmVyIHRydXN0IGEgY29tcHV0ZXIgeW91IGNhbid0IHRocm93IG91dC
 
 var hDecodedString = 'hDecodedString: \"Never trust a computer you can\'t throw out a window\" - Steve Wozniak';
 
+// https://developer.mozilla.org/en-US/docs/Web/API/WindowBase64/Base64_encoding_and_decoding
+
+// each char in this string is treated as a byte of binary data
+// base64 representation of string is returned
 console.log('encodedString:\n', btoa(string), '\n');
+
+// atob decodes a base64 encoded string
 console.log('decodedString:\n', atob(encodedString), '\n');
+
 console.log('hDecodedString:\n', atob(hEncodedString), '\n');
